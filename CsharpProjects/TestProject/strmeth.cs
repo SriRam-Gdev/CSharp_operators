beaf53aa7bf7c32d3f014a019521ab73495bd077
Sriram s <123556950+SriRam-Gdev@users.noreply.github.com>
2024-09-06 15:40:03 +0530

    first commit

---
+++ CsharpProjects/TestProject/strmeth.cs
@@ -0,0 +1,8 @@
+﻿ string pangram = "The quick brown fox jumps over the lazy dog.";
+// Console.WriteLine(pangram.Contains("fox"));
+// Console.WriteLine(pangram.Contains("cow"));
+
+// These two lines of code will create the same output
+
+Console.WriteLine(pangram.Contains("fox")==true);
+Console.WriteLine(!pangram.Contains("fox"));
---
+++ operator.cs
@@ -0,0 +1,25 @@
+﻿
+// //equality operstor 
+Console.WriteLine("a" == "a"); 
+Console.WriteLine("a" == "A");
+Console.WriteLine(1 == 2);
+
+string value = "a";
+Console.WriteLine(value == "a");
+
+// // example
+
+string str = "man ";
+string str2 = " MAn";
+Console.WriteLine(str.Trim().ToLower() == str2.Trim().ToLower());
+
+//inequality operator
+
+Console.WriteLine("a" != "a");
+Console.WriteLine("a" != "A");
+Console.WriteLine(1 != 2);
+
+string myValue = "a";
+Console.WriteLine(myValue != "a");
+
+
